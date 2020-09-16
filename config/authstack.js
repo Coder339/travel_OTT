14180f70d930dfe85848c2b9ffc405c1eefd73ef
Rohit Ninawe <rohit.ninawe@travelxp.tv>
2020-09-16 16:01:35 +0530

    git push

---
+++ config/authstack.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import AuthContext from '../config/authcontext';
+import Amplify from '@aws-amplify/core';
+import Auth from '@aws-amplify/auth';
+import aws_exports from '../config/aws-exports';
+import Home from '../views/home';
+import Login from '../views/login';
+
+Amplify.configure(aws_exports);
+
+export default class AuthStack extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userToken: undefined,
+            user: {
+                name: '',
+                said: null,
+                given_name: '',
+                phone_number: '',
+                phone_country_code: ''
+            },
+            device: device,
+            os: os,
+            signOut: this.signOut,
+            updateUser: this.updateUser
+        };
+    }
+
+    async componentDidMount() {
+        Auth.currentAuthenticatedUser()
+            .then((res) => {
+                let said = 'custom:said';
+                let countrycode = 'custom:phone_country_code';
+                setTimeout(() => {
+                    this.setState({
+                        user: {
+                            name: res.attributes.name,
+                            said: res.attributes[said],
+                            given_name: res.attributes.given_name,
+                            phone_number: res.attributes.phone_number,
+                            phone_country_code: res.attributes[countrycode]
+                        },
+                        userToken: res.signInUserSession.idToken.jwtToken
+                    })
+                }, 3000)
+            })
+            .catch((e) => {
+                if (e == 'not authenticated') {
+                    setTimeout(() => {
+                        this.setState({
+                            userToken: null
+                        })
+                    }, 3000)
+                }
+            })
+    }
+
+    signOut = () => {
+        Auth.signOut()
+            .then((res) => {
+                this.setState({
+                    userToken: null,
+                })
+            })
+            .catch(res => console.log(res))
+    }
+
+    updateUser = (res) => {
+        let token = res.signInUserSession.idToken.jwtToken;
+        let said = 'custom:said';
+        let countrycode = 'custom:phone_country_code';
+        this.setState({
+            user: {
+                ...this.state.user,
+                name: res.attributes.name,
+                said: res.attributes[said],
+                given_name: res.attributes.given_name,
+                phone_number: res.attributes.phone_number,
+                phone_country_code: res.attributes[countrycode]
+            },
+            userToken: token,
+        })
+    }
+
+    render() {
+        const authProviderValue = {
+            ...this.state,
+        };
+        return (
+            <AuthContext.Provider value={authProviderValue}>
+                {this.state.userToken === undefined ?
+                    <Welcome />
+                    :
+                    this.state.userToken === null ?
+                        <Login />
+                        :
+                        <Home />
+                }
+            </AuthContext.Provider>
+        );
+    }
+}
